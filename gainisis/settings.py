9960979f85985b8cf2fbfea8bb244d862a8bce57
Nnaemeka Chukwuemeka <mekkynworu@gmail.com>
2023-05-22 12:31:47 +0100

    dotenv

--- gainisis/settings.py
+++ gainisis/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = str(os.getenv('MEKGAIN_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['3.86.85.194', 'localhost',]
+ALLOWED_HOSTS = ['54.167.182.239', 'localhost',]
 
 
 # Application definition
